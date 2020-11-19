a42a1ca6ce7b55ff2ffcde2c265ba0367347b4b6
Matheus Cruz Souza <matheuscruzsouza@gmail.com>
2020-11-19 14:21:40 -0300

    Start build select

--- src/app/documentacao/documentacao.component.ts
+++ src/app/documentacao/documentacao.component.ts
@@ -17,12 +17,25 @@ export class DocumentacaoComponent implements OnInit {
       validators: [Validators.required, Validators.email],
       updateOn: "blur",
     }),
+    select: new FormControl(""),
   });
 
   @ViewChild(WindowComponent) janela: WindowComponent;
 
   valor = 3;
   secondaryWindow = false;
+  options = [
+    { label: "A" },
+    { label: "B" },
+    { label: "C" },
+    { label: "D" },
+    { label: "E" },
+    { label: "F" },
+    { label: "G" },
+    { label: "H" },
+    { label: "I" },
+    { label: "J" },
+  ];
 
   constructor(private formBuilder: FormBuilder) {}
 
